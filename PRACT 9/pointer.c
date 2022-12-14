b2862acb6d13eea4c260c855144e1545a8b8990c
StevenAMC <steven_mc8947@hotmail.com>
2022-12-14 04:37:39 -0500

    up

--- PRACT 9/pointer.c
+++ PRACT 9/pointer.c
@@ -4,15 +4,22 @@
 #include <string.h>
 #include <math.h>
 
-const uint8_t offs = 25;
+
+void sumalo(uint8_t *num);
 
 void main(void)
 
 {
-    printf("0x%X\n",&offs);
+    uint8_t offs = 25;
+    uint8_t *p = &offs;
+    printf("%i\n",offs);
+    //printf("0x%X\n",&offs);
+    sumalo(p);
+    printf("%p\n",&offs);
+    printf("%i\n",offs);
 
 }
 // Hola git
-void sumalo(){
-    
+void sumalo(uint8_t *num){
+    *num = *num + 1;
 }

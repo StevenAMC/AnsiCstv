fa9ae9a2d29c3affd1b0a290f86cd1cb8ecec4df
StevenAMC <steven_mc8947@hotmail.com>
2022-12-15 14:24:05 -0500

    up15/12

--- PRACT 9/point2.c
+++ PRACT 9/point2.c
@@ -1,1 +1,26 @@
-//*HW
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+uint16_t sueldos[5] = {1400,1025,1700,2000,3600};
+uint16_t *pt = NULL;
+
+int main(void){
+    pt = sueldos; //* El puntero apunta a la dir de memoria del primer elemento del arrelo
+    printf("Sueldos[0]: %u\n",*pt);
+    pt++;
+    printf("Sueldos[1]: %u\n",*pt);
+    pt+=2;
+    printf("Sueldos[3]: %u\n",*pt);
+    *pt=100;
+    printf("Sueldos[3]: %u\n",*pt);
+    pt++;
+    printf("Sueldos[4]: %u\n",*pt);
+    pt-=4;
+    *pt =1;
+    printf("Sueldos[0]: %u\n",*pt);
+    
+    
+}

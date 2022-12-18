d0c79ab0f5bf5160b519d74f34d09acd0395a79c
StevenAMC <steven_mc8947@hotmail.com>
2022-12-17 23:20:05 -0500

    ptrcscst

--- PRACT 9/pointerConst.c
+++ PRACT 9/pointerConst.c
@@ -4,8 +4,35 @@
 
 int x = 10, y = 16;
 int *const ptrConst = &x;
+//* Punteros Constantes
+uint8_t PORTD,PORTC;
+uint8_t *const ptrPD = &PORTD;
+
+//* Punteros a constantes
+uint8_t data1 = 22, data2 = 55;
+const uint8_t *ptrCst = &data1;
+//* Punteros constantes a constantes
+const uint8_t datac1 = 12,datac2;
+
+const uint8_t *const ptrCstCst = &datac1;
+
 
 void main(void){
-    ptrConst = &y;
-    ptrConst++;
+    //* Cst cst can only read, cant chage direction
+    printf("Valor cst cst : %u\n",*ptrCstCst);
+
+
+    //! *ptrCst = 22;//ERROR
+    ptrCst = &data2;
+    ptrCst = &data1;
+    
+    *ptrConst = 12; 
+    
+
+    printf("Valor de x = %i\n",x);
+    // ptrConst = &y;
+    // ptrConst++;
+    *ptrPD = 0xBA;
+    printf("Valor de PORTD = %X\n",*ptrPD);
+
 }
